b504133f0d5731b03ac97c3a04e870bbea00d4df
Maxwell DeMaio <maxdman321@gmail.com>
2021-01-12 23:11:17 -0500

    Created sfcs and removed non-used files

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+// Stateless functional component
+const Home = () => {
+    return (  
+        <div className="home">
+            <h2>Homepage</h2>
+        </div>
+    );
+}
+ 
+export default Home;
