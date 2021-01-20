af26a11b1516dd0360beee7b2bff22c395c75f2e
Maxwell DeMaio <maxdman321@gmail.com>
2021-01-19 23:58:52 -0500

    Started adding controlled inputs with state for create route

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('max');
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="max">max</option>
+                    <option value="alice">alice</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
         </div>
      );
 }
